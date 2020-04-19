392d74e9e113b09c36b1ca06797777e631657133
Sampath Kumar Ajjapaga <sampathkumar.ajjapaga@gmail.com>
2020-04-19 07:55:46 +0530

    with out bar-chart

---
+++ scripts/canvas.js
@@ -0,0 +1,33 @@
+let canvas = document.querySelector("#bar-chart");
+let cc = canvas.parentElement;
+var ctx =  canvas.getContext("2d");
+
+
+function draw(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.onresize = function(){
+    canvas.width = cc.clientWidth;
+    canvas.height = cc.clientHeight;
+
+
+}
