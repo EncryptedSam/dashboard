745217492a0e480f4867849c5fe642f3ada2e0d0
Sampath Kumar Ajjapaga <sampathkumar.ajjapaga@gmail.com>
2020-04-19 09:26:59 +0530

    graph added

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,33 +1,44 @@
-let canvas = document.querySelector("#bar-chart");
-let cc = canvas.parentElement;
-var ctx =  canvas.getContext("2d");
+window.onload = function () {
 
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        axisY: {
+            title: "Amount",
+            suffix: "k",
+            gridThickness: 0
+        },
+        axisX: {
+            title: "Monthly",
+            gridThickness: 0
+        },
+        dataPointWidth: 20,
+        data: [{
+            color: "#4FA953",
+            type: "column",
+            // yValueFormatString: "#,##0.0#\"%\"",
+            dataPoints: [
+                { label: "Jan", y: 150},	
+                { label: "Feb", y: 100 },	
+                { label: "Mar", y: 250 },
+                { label: "Apr", y: 120 },	
+                { label: "May", y: 310 },
+                { label: "Jun", y: 190 },
+                { label: "Jul", y: 350 },
+                { label: "Aug", y: 210 },
+                { label: "Seb", y: 160 },
+                { label: "Oct", y: 250 },
+                { label: "Nov", y: 10 },
+                { label: "Dec", y: 300 }
+                
+            ]
+        }]
+    });
+    chart.render();
 
-function draw(){
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-window.onresize = function(){
-    canvas.width = cc.clientWidth;
-    canvas.height = cc.clientHeight;
-
+    let canvasjsChartContainer = document.querySelector(".canvasjs-chart-container");
+    if(canvasjsChartContainer){ 
+        let canv = canvasjsChartContainer.querySelectorAll("canvas");
+        canv[1].style.position = "relative";
+    }
 
 }
